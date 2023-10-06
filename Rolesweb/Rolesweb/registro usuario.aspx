88df12b51dc8a78d44d89ade63567188a39e4ff4
JilmarPalaMolina <100879730+JilmarPalaMolina@users.noreply.github.com>
2023-10-05 20:24:52 -0500

    comit1
    ...

---
+++ Rolesweb/Rolesweb/registro usuario.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="registro usuario.aspx.cs" Inherits="Rolesweb.registro_usuario" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <link href="estilos.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <center>
+            <br />
+            <div class="div">
+                <br />
+                <h3>Formulario de registro de usuarios</h3>
+                <asp:TextBox ID="txt_codigo" runat="server" placeholder="Codigo" CssClass="cajas"></asp:TextBox>               
+                <asp:TextBox ID="txt_Pnombre" runat="server" placeholder="Primer Nombre" CssClass="cajas"></asp:TextBox>          
+                <asp:TextBox ID="txt_Snombre" runat="server" placeholder="Segundo Nombre" CssClass="cajas"></asp:TextBox>
+                <asp:TextBox ID="txt_Papellido" runat="server" placeholder="Primer Apellido" CssClass="cajas"></asp:TextBox>
+                <asp:TextBox ID="txt_Sapellido" runat="server" placeholder="Segundo apellido" CssClass="cajas"></asp:TextBox>
+                <asp:TextBox ID="txt_contacto" runat="server" placeholder="Contacto" CssClass="cajas"></asp:TextBox>
+                <asp:TextBox ID="Txt_correo" runat="server" placeholder="Correo" CssClass="cajas"></asp:TextBox>
+                 <asp:TextBox ID="txt_sexo" runat="server" placeholder="sexo" CssClass="cajas"></asp:TextBox>
+                <asp:Button ID="btn_registrar" runat="server" Text="Registrar" CssClass="botones"/>
+               
+                <br />
+                <asp:Label ID="lbl_mensaje" runat="server" Text=""></asp:Label>
+            </div>
+        </center>
+    </form>
+</body>
+</html>
---
+++ Rolesweb/Rolesweb/registro usuario.aspx.designer.cs
@@ -0,0 +1,116 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Rolesweb
+{
+
+
+    public partial class registro_usuario
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control txt_codigo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_codigo;
+
+        /// <summary>
+        /// Control txt_Pnombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_Pnombre;
+
+        /// <summary>
+        /// Control txt_Snombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_Snombre;
+
+        /// <summary>
+        /// Control txt_Papellido.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_Papellido;
+
+        /// <summary>
+        /// Control txt_Sapellido.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_Sapellido;
+
+        /// <summary>
+        /// Control txt_contacto.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_contacto;
+
+        /// <summary>
+        /// Control Txt_correo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Txt_correo;
+
+        /// <summary>
+        /// Control txt_sexo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_sexo;
+
+        /// <summary>
+        /// Control btn_registrar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_registrar;
+
+        /// <summary>
+        /// Control lbl_mensaje.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl_mensaje;
+    }
+}
